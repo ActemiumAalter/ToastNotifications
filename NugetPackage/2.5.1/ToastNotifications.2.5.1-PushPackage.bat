43d2316485558ae4a55be76fa6792c298e0a46c5
Tobias Nuyttens <tobias.nuyttens@vinci-energies.net>
2020-06-05 09:21:01 +0200

    Add files for pushing the package to tfs feed

---
+++ NugetPackage/2.5.1/ToastNotifications.2.5.1-PushPackage.bat
@@ -0,0 +1,2 @@
+nuget.exe push -Source "https://tfs.actemium.be/tfs/DefaultCollection/_packaging/ThirdParty/nuget/v3/index.json" -ApiKey AzureDevOps ToastNotifications.2.5.1.nupkg
+pause
---
+++ NugetPackage/2.5.1/ToastNotifications.Messages.2.5.1-PushPackage.bat
@@ -0,0 +1,2 @@
+nuget.exe push -Source "https://tfs.actemium.be/tfs/DefaultCollection/_packaging/ThirdParty/nuget/v3/index.json" -ApiKey AzureDevOps ToastNotifications.Messages.2.5.1.nupkg
+pause
